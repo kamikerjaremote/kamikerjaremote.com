eaecb44b66301971bab67d61b6116a54cb1f3949
Agung Sundoro <muhagung2001@gmail.com>
2024-09-12 22:22:59 +0700

    fix: base url should be direct

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ import adapter from "@sveltejs/adapter-static";
 const config = {
 	kit: {
 		adapter: adapter(),
-		paths: { base: "/kamikerjaremote.github.io" },
+		paths: { base: "/" },
 	},
 };
 
